0cf43be87d2e93c5363786b797faa3077608c077
fei <1450622282@qq.com>
2014-05-24 02:02:52 -0400

    steps
    Signed-off-by: fei <1450622282@qq.com>

--- step2/app.js
+++ step2/app.js
@@ -22,6 +22,29 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.get('/hello', routes.hello);
-app.get('/user/:username', routes.user);
+var users = {
+	'jason': {
+		'name': 'Jason',
+		'website': 'https://www.google.com'
+	},
+	'fei': {
+		'name': 'Fei',
+		'website': 'https://www.facebook.com'
+	}
+};
+app.all('/user/:username', function(req, res, next){
+	if(users[req.params.username]){
+		next();
+	}
+	else{
+		res.send(req.params.username + ' does not exist');
+	}
+});
+app.get('/user/:username', function(req, res){
+	res.send(JSON.stringify(users[req.params.username]));
+});
+app.put('/user/:username', function(req, res){
+	res.send('Done');
+});
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
